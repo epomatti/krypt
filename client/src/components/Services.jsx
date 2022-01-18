333b7448efcd66d8a25cfd0e1c9951d437c125b4
epomatti <evandro.pomatti@gmail.com>
2022-01-18 14:15:39 -0300

    transactins, footer

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -16,10 +16,10 @@ const ServiceCard = ({ color, title, icon, subtitle }) => (
 
 const Services = () => {
   return (
-    <div className='flex flex-col md:flex-rol w-full justify-center items-center gradient gradient-bg-services'>
-      <div className='flex mf:flex-row flex-column items-center justify-between mp:p-200 py-20 px-4'>
-        <div className='flex-1 flex flex-col justify-start items-start'>
-          <h1 className='text-white text-3xl sm:text-5xl py-2 text-gradient'>Services that we
+    <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services">
+    <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+      <div className="flex-1 flex flex-col justify-start items-start">
+        <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
             <br />
             continue to improve
           </h1>
